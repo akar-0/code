4a195d204d0c66c898608897ef59470968232c7a
akar-0 <68073164+akar-0@users.noreply.github.com>
2020-12-23 13:32:36 +0100

    Create prime_decomposition.py

---
+++ py/prime_decomposition.py
@@ -0,0 +1,18 @@
+from itertools import circle
+
+def factors(n):
+    if is_prime(n):return {n:1}
+    L=[]
+    while not n%2:
+        L.append(2)
+        n = n>>1
+    for p in (3,5):
+        while not n%p:
+            L.append(p)
+            n //=p
+    else: p += 2
+    i=iter(cycle((4, 2, 4, 2, 4, 6, 2, 6)))
+    while n != 1:
+        while not n%p:
+            L.append(p)
+            n //=p
