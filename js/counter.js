b03ee5ef52ca80c8d618460d8bb7355114d87487
akar-0 <68073164+akar-0@users.noreply.github.com>
2021-01-01 03:49:36 +0100

    Update counter.js

--- js/counter.js
+++ js/counter.js
@@ -12,3 +12,6 @@ function counter(e){
 
 //on the fly
 counter(val, arr){return arr.filter(e => e == val).length}
+
+//https://www.codewars.com/kata/reviews/59106fc3c37bca02fc000981/groups/5f80d9ed86498900011ee79d
+const cnt=a=>a.reduce((o,n)=>(o[n]=o[n]+1||1,o), {});
