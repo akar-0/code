0a39732315de3db4e11b789a8766ffb82b7622a8
akar-0 <68073164+akar-0@users.noreply.github.com>
2021-01-03 00:55:53 +0100

    Update rotate_matrix.js

--- js/rotate_matrix.js
+++ js/rotate_matrix.js
@@ -1,4 +1,6 @@
 //https://www.codewars.com/kata/582c01ad3fd1cc5736000348/train/javascript
+//see also http://www.codesuck.com/2012/02/transpose-javascript-array-in-one-line.html
+//https://www.codewars.com/kata/reviews/525a5cef85a9a47bc80006b0/groups/55009212c502956b41000c85
 const _=require("lodash");
 
 const rotateMatrix = arr => _.zip(...arr).reverse();
