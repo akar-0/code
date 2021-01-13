d117b22b70f343b1049a9678d51dd79fc28e9b6f
akar-0 <68073164+akar-0@users.noreply.github.com>
2021-01-13 13:34:54 +0100

    Update compare_arrays.js

--- js/compare_arrays.js
+++ js/compare_arrays.js
@@ -1,2 +1,3 @@
+//https://www.codewars.com/kata/589a9792ea93aae1bf00001c/solutions/javascript
 //https://www.codewars.com/kata/57a23e3753ba332b8e0008da/solutions/javascript
 key.every((n,i)=>n===key[key.length-1-i])
