7546d0e270aeb7f9cfb508e80b87796c56eb8f66
akar-0 <68073164+akar-0@users.noreply.github.com>
2021-01-30 23:09:04 +0100

    Create split_string.cpp

---
+++ cpp/split_string.cpp
@@ -0,0 +1,16 @@
+//https://stackoverflow.com/questions/14265581/parse-split-a-string-in-c-using-string-delimiter-standard-c
+vector<string> split(const string& str, const string& delim)
+{
+    vector<string> tokens;
+    size_t prev = 0, pos = 0;
+    do
+    {
+        pos = str.find(delim, prev);
+        if (pos == string::npos) pos = str.length();
+        string token = str.substr(prev, pos-prev);
+        if (!token.empty()) tokens.push_back(token);
+        prev = pos + delim.length();
+    }
+    while (pos < str.length() && prev < str.length());
+    return tokens;
+}
