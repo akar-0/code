49573f50cf4fbd5b21c6eb4bfc460cd91e53d346
akar-0 <68073164+akar-0@users.noreply.github.com>
2021-02-03 21:57:54 +0100

    Update reduce.cpp

--- cpp/reduce.cpp
+++ cpp/reduce.cpp
@@ -1,4 +1,4 @@
-#See https://www.codewars.com/kata/58cb43f4256836ed95000f97/solutions/cpp
+//See https://www.codewars.com/kata/58cb43f4256836ed95000f97/solutions/cpp https://www.codewars.com/kata/5a905c2157c562994900009d/solutions/cpp
 #include <numeric>
 std::accumulate(a.begin(), a.end(),0) //sum
 std::accumulate(a.begin(), a.end(), 1, std::multiplies<int>()) //mul
