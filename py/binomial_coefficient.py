b13a0b1f8e5966f3acb43282e21bea1aa8d5d954
akar-0 <68073164+akar-0@users.noreply.github.com>
2021-02-07 11:21:20 +0100

    Create binomial_coefficient.py

---
+++ py/binomial_coefficient.py
@@ -0,0 +1,12 @@
+from scipy.special import binom
+
+#???
+from math import factorial as fact
+def f(k,n):
+    res=1
+    for x in range(k+1,n+1):
+        res*=x
+    else: return res
+def c(n,k):
+    res=
+    return f(k,n)//fact(n-k)
