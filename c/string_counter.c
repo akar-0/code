b315504c13b43eb8d7d46203033c5119b949a107
akar-0 <68073164+akar-0@users.noreply.github.com>
2021-02-08 20:53:30 +0100

    Create string_counter.c

---
+++ c/string_counter.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <string.h>
+
+bool permute_a_palindrome(const char *s) {
+  int n, count[26]={-1}, i=0,c,l=strlen(s);
+  if (l==1){return true;}
+  bool flag=false;
+  while (s[i]!='\0'){
+    n = s[i] - 'a';
+    if (count[n]==-1){count[n]=1;}
+    else {count[n]++;}
+    i++;
+  }
+  for (i=0;i<26;i++){
+    c=count[i];
+    if (c>0&&c%2){
+      if (flag){return false;}
+      else {flag=true;}
+    }
+  }
+ return true;
+}
