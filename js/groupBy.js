85994526bcf55b937770da6115427afb649289f9
akar-0 <68073164+akar-0@users.noreply.github.com>
2021-02-08 11:29:55 +0100

    Create groupBy.js

---
+++ js/groupBy.js
@@ -0,0 +1,30 @@
+function groupBy(L){
+  const res=[]
+  var i=0
+  while (i<L.length){
+    const n=L[i], tmp=[n,[n]]
+    while (L[i+1]===n){
+      i++
+      tmp[1].push(n)
+    }
+    res.push(tmp)
+    i++
+  }
+ return res
+}
+
+function groupBy(L){
+  const res=[]
+  var i=0
+  while (i<L.length){
+    const n=L[i]
+    var c=0
+    while (L[i+1]===n){
+      i++
+      c++
+    }
+    res.push([n,c])
+    i++
+  }
+ return res
+}
