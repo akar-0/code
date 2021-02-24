0a7d7c11640ff90cfe57d24b9a1efb65ae2080d5
akar-0 <68073164+akar-0@users.noreply.github.com>
2021-02-24 13:54:57 +0100

    Update prime_test.cpp

--- cpp/prime_test.cpp
+++ cpp/prime_test.cpp
@@ -12,3 +12,34 @@ bool isPrime(int n)
   }
    return true;
 }
+
+#include <vector>
+#include <numeric>
+
+
+using namespace std;
+
+bool isPrime(int n){
+  if (n<2){return false;}
+  if (n==2||n==3||n==5||n==5){return true;}
+  if (!(n%2)||!(n%3)||!(n%5)){return false;}
+  vector<int> c={4, 2, 4, 2, 4, 6, 2, 6};
+  int p=7, i=0;
+  while (p*p<=n)
+    {
+    if (!(n%p)){return false;}
+    p+=c[i];
+    i=(i+1)%8;
+  }
+ return true;
+}
+
+int nextPrime(int n){
+ if (n <2){return 2;}
+  if (n==2){return 3;}
+  if (!(n%2)){n++;}
+  else {n+=2;}
+  while (1){
+    if (isPrime(n)){return n;}
+    else {n+=2;}}
+}
