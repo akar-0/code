00e5e1062576eb58328f6b75bbb09bd7ee6e7937
akar-0 <68073164+akar-0@users.noreply.github.com>
2021-02-24 13:25:51 +0100

    Update prime_decomposition.js

--- js/prime_decomposition.js
+++ js/prime_decomposition.js
@@ -52,10 +52,10 @@ function primeFactors(n){
   if (m>1){return {n:1}}
   return counter(arr)
 }
-
 function isPrime(n){
+  if (n<2){return false}
   if ([2,3,5,7].includes(n)){return true}
-  if (n==1||!(n%2)||!(n%3)||!(n%5)){return false}
+  if (!(n%2)||!(n%2)||!(n%3)||!(n%5)){return false}
   const c=[4, 2, 4, 2, 4, 6, 2, 6]
   var p=7, i=0
   while (p*p<=n){
