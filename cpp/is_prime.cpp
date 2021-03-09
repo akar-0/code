d22f6f68da92a7284cb78b429c19451ceba51fdb
akar-0 <68073164+akar-0@users.noreply.github.com>
2021-03-09 09:21:52 +0100

    Create is_prime.cpp

---
+++ cpp/is_prime.cpp
@@ -0,0 +1,15 @@
+#include <cmath>
+
+bool isPrime(int n) {
+  if (n<2){return false;}
+  if (n==2 || n==3 || n==5 || n==7){return true;}
+  if (!(n%2)||!(n%3)||!(n%5)){return false;}
+  int c[8]={4, 2, 4, 2, 4, 6, 2, 6};
+  long p=7, i=0;
+  while (p*p<=n){
+    if ((n%p)==0){return false;}
+    p += c[i];
+    i = (i+1)%8;
+  }
+ return true;
+}
