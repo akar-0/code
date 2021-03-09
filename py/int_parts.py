2a9b2dda6695a27d08483c7a555e4e3c469b4e95
akar-0 <68073164+akar-0@users.noreply.github.com>
2021-03-09 17:03:56 +0100

    Update int_parts.py

--- py/int_parts.py
+++ py/int_parts.py
@@ -1,12 +1,11 @@
 #https://www.codewars.com/kata/5716a4c2794d305f4900156b/solutions/python
 def find_part_max_prod(n):
-    a,b,c,d=[0]*(n+1),1,n-1,defaultdict(set)
+    a,b,c,s=[0]*(n+1),1,n-1,set()
     while b:
         h=a[b-1]+1
         b-=1
         while 2*h<=c: a[b]=h; c-=h; b+=1
         else: l=b+1
-        while h<=c: a[b]=h; a[l]=c; e=tuple(a[:b+2][::-1]); d[prod(e)].add(e); h+=1; c-=1
-        else: a[b]=h+c; e=tuple(a[:b+1][::-1]); d[prod(e)].add(e); c+=h-1
-    m=max(d)
-    return sorted(map(list,d[m]),reverse=True)+[m]
+        while h<=c: a[b]=h; a[l]=c; e=tuple(a[:b+2][::-1]); s.add(e); h+=1; c-=1
+        else: a[b]=h+c; e=tuple(a[:b+1][::-1]); s.add(e); c+=h-1
+    else: return s
