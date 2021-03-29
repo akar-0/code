cb9b3502c6c74725720e0e546fdfdf8b7474972b
akar-0 <68073164+akar-0@users.noreply.github.com>
2021-03-29 20:01:27 +0200

    Update prime_test.rs

--- rust/prime_test.rs
+++ rust/prime_test.rs
@@ -8,3 +8,19 @@ fn is_prime(x: i64) -> bool {
     }
     return true;
 }
+
+
+fn isPrime(x: u64) -> bool {
+    if x<2 {return false;}
+    if x ==2 || x==3 || x == 5 || x == 7 {return true;}
+    if x%2==0 || x%3==0 || x%5==0 {return false;}
+    let c = [4, 2, 4, 2, 4, 6, 2, 6];
+    let mut p = 7;
+    let mut i=0;
+    while p*p <= x {
+        if x%p == 0 {return false;}
+        else {p += c[i];}
+        i = (i+1)%8;
+    }
+    return true;
+}
