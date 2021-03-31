623f4f9086df290b3811ae0a99816c28b575c13c
akar-0 <68073164+akar-0@users.noreply.github.com>
2021-03-31 07:31:25 +0200

    Update prime_decomposition.py

--- py/prime_decomposition.py
+++ py/prime_decomposition.py
@@ -15,3 +15,24 @@ def factors(n):
         while not n%p: L.append(p); n //=p
         else: p += next(i)
     else: return Counter(L)
+
+from itertools import cycle
+from gmpy2 import is_prime
+from collections import Counter
+from functools import lru_cache
+from collections import defaultdict
+    
+@lru_cache(maxsize=None)
+def factors(n):
+    if is_prime(n):return {n:1}
+    d=defaultdict(int)
+    for p in (2,3,5):
+        while not n%p:
+            d[p]+=1
+            n//=p
+    else: p += 2
+    i=iter(cycle((4, 2, 4, 2, 4, 6, 2, 6)))
+    while n != 1:
+        while not n%p: d[p]+=1; n //=p
+        else: p += next(i)
+    else: return d
