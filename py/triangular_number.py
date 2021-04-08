e1c031431b6e8fc5a8740e47db875fc368375f14
akar-0 <68073164+akar-0@users.noreply.github.com>
2021-04-08 18:12:58 +0200

    Create triangular_number.py

---
+++ py/triangular_number.py
@@ -0,0 +1,8 @@
+def is_triangular(n):
+    if n < 0:return False
+    c= -2*n
+    a=b=1
+    d=b*b-4*a*c
+    if (d < 0): return False
+    e=d**.5
+    return any(x>0 and x==int(x) for x in ((-b+e)/(2*a),(-b-e)/(2*a)))
