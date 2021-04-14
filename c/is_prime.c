4198775b66f3bd776fd41639c878f7157f434cf9
akar-0 <68073164+akar-0@users.noreply.github.com>
2021-04-14 14:06:41 +0200

    Update is_prime.c

--- c/is_prime.c
+++ c/is_prime.c
@@ -25,3 +25,56 @@ int nextPrime(int n){
     n+=2;
   }
 }
+
+
+
+//A retoucher (renvoie une string (p1**i1)...)
+#include <math.h>
+#include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+char* factors(int n) {
+  char* o=calloc(2000,sizeof(char));
+  int p=3, f=0, s=sqrt(n);
+    while (!(n%2)){
+      f++;
+      n/=2;
+    }
+    if (f==1){
+      char* tmp=calloc(4,sizeof(char));
+      sprintf(tmp,"(2)");
+      strcat(o,tmp);
+    }
+    else if (f) {
+      char* tmp=calloc(100,sizeof(char));
+      sprintf(tmp,"(2**%d)",f);
+      strcat(o,tmp);
+    }
+  while (p*p<=n){
+    f=0;
+    while (!(n%p)){
+      f++;
+      n/=p;
+    }
+    if (f){
+      char* tmp=calloc(100,sizeof(char));
+      if (f==1){
+        sprintf(tmp,"(%d)",p);
+        strcat(o,tmp);
+      }
+      else {
+        sprintf(tmp,"(%d**%d)",p,f);
+        strcat(o,tmp);
+      }
+    }
+    p+=2;
+  }
+  if (n!=1){
+   char* tmp=calloc(100,sizeof(char));
+   sprintf(tmp,"(%d)",n);
+   strcat(o,tmp);
+  }
+  o[strlen(o)]='\0';
+  return o;
+}
