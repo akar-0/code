a58873a86fe721659b7d94c510e63f98bb8dcd1f
akar-0 <68073164+akar-0@users.noreply.github.com>
2021-04-17 01:20:05 +0200

    Update counter.rs

--- rust/counter.rs
+++ rust/counter.rs
@@ -1,1 +1,2 @@
 // https://stackoverflow.com/questions/64178272/what-is-the-idiomatic-rust-way-to-build-a-hashmap-of-character-counts
+// https://www.codewars.com/kata/54bf1c2cd5b56cc47f0007a1/solutions/rust 
