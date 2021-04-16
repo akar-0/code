735c094b8b68b0692709b0423ebc1fb460b8cfc6
akar-0 <68073164+akar-0@users.noreply.github.com>
2021-04-16 16:57:49 +0200

    Update string_to_int.c

--- c/string_to_int.c
+++ c/string_to_int.c
@@ -4,3 +4,4 @@
 // https://www.codewars.com/kata/56a4872cbb65f3a610000026/solutions
 // https://www.codewars.com/kata/56541980fa08ab47a0000040/solutions/c
 // https://www.codewars.com/kata/5265326f5fda8eb1160004c8/solutions
+// https://www.codewars.com/kata/51ba717bb08c1cd60f00002f/solutions/c
