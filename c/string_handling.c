c181fa27e4c6221a8cc957df20b697ef1f3bea90
akar-0 <68073164+akar-0@users.noreply.github.com>
2021-04-17 16:28:24 +0200

    Create string_handling.c

---
+++ c/string_handling.c
@@ -0,0 +1,14 @@
+// strtok
+// https://www.tutorialspoint.com/c_standard_library/c_function_strtok.htm
+// https://koor.fr/C/cstring/strtok.wp
+// http://www.cplusplus.com/faq/sequences/strings/strtok/
+// find
+// https://stackoverflow.com/questions/10844629/string-find-in-c/10844649
+// find last of
+// https://www.tutorialspoint.com/c_standard_library/c_function_strrchr.htm
+// https://www.tutorialspoint.com/c_standard_library/c_function_strchr.htm
+// substr
+// https://stackoverflow.com/questions/4214314/get-a-substring-of-a-char
+// https://stackoverflow.com/questions/2114377/strings-in-c-how-to-get-substring
+// trim
+// https://stackoverflow.com/questions/122616/how-do-i-trim-leading-trailing-whitespace-in-a-standard-way
