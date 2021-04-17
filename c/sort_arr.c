a452da74ab8c02035fb15aa892e5f068f8b29c93
akar-0 <68073164+akar-0@users.noreply.github.com>
2021-04-18 01:37:40 +0200

    Update sort_arr.c

--- c/sort_arr.c
+++ c/sort_arr.c
@@ -1,2 +1,7 @@
 // https://www.codewars.com/kata/5f55ecd770692e001484af7d/solutions/c
 // https://www.codewars.com/kata/550498447451fbbd7600041c/solutions
+int comp(const void * a, const void * b) {
+   return (*(int*)a - *(int*)b);
+}
+
+  qsort(array, size_arr, sizeof(int), comp);
