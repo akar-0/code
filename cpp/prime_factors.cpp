50e59b2277dbaeeecca6d9ad0403e2d35703d154
akar-0 <68073164+akar-0@users.noreply.github.com>
2021-04-24 14:23:30 +0200

    Update prime_factors.cpp

--- cpp/prime_factors.cpp
+++ cpp/prime_factors.cpp
@@ -1,5 +1,45 @@
 using namespace std;
 
+
+#include <unordered_map>
+#include <vector>
+#include <cmath>
+
+using namespace std;
+unordered_map<int,unsigned> factors(int n) {
+  vector<int> arr;
+  unordered_map<int, unsigned> count;
+ // if (n<2) {return unordered_map<int, unsigned>();}
+  int m=n, p=7, i=0;
+  while (m%2 == 0){
+    arr.push_back(2);
+    m /= 2;
+  }
+  while (m%3 == 0){
+    arr.push_back(3);
+    m /= 3;
+  }
+  while (m%5 == 0){
+    arr.push_back(5);
+    m /= 5;
+  }
+  vector<int> c={4, 2, 4, 2, 4, 6, 2, 6};
+  while (m!=1 || p*p<=n) {
+    while (!(m%p)){
+      m/=p;
+    arr.push_back(p);
+    }
+    p+=c[i];
+    i=(i+1)%8;
+  }
+  if (m>1) {arr.push_back(n);}
+  for (int k: arr) {count[k]++;}
+  return count;
+}
+
+
+
+
 std::vector<int> prime_factors(long n){
   if (isPrime(n)) //A FINIR
   vector< int > arr;
