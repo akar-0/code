a007003383b4f6ae8bd0d47b2a3c1b0541bfdd11
akar-0 <68073164+akar-0@users.noreply.github.com>
2021-04-24 13:49:45 +0200

    Update prime_decomposition.js

--- js/prime_decomposition.js
+++ js/prime_decomposition.js
@@ -1,45 +1,14 @@
 
-function primeFactors(n){
-  let m=n, arr=[]
-  for (const p of [2,3,5]){
-    if (!(m%p)){
-        let e=1
-        m /=p
-        while (!(m%p)){
-          e++
-          m /= p 
-          }
-        arr.push([p, e])
-        }
-  }
-  let p=7, i=0
-  const c=[4, 2, 4, 2, 4, 6, 2, 6]
-  while (m != 1 || p*p<n){
-    if (!(m%p)){
-        var e = 1
-        m /=p
-        while (!(m%p)){
-          e++
-          m /= p 
-          }
-        arr.push([p, e])
-        }
-    p += c[i]
-    i = (i+1)%8
-  }
-}
-
-
-function primeFactors(n){
-  var m=n
-  var arr=[]
+function factors(n){
+  let m=n
+  const arr=[]
   for (const p of [2,3,5]){ 
         while (!(m%p)){
           m /= p
           arr.push(p)
         }
   }
-  var p=7, i=0
+  let p=7, i=0
   const c=[4, 2, 4, 2, 4, 6, 2, 6]
   while (m != 1 || p*p<n){
         while (!(m%p)){
@@ -52,6 +21,8 @@ function primeFactors(n){
   if (m>1){return {n:1}}
   return counter(arr)
 }
+
+
 function isPrime(n){
   if (n<2){return false}
   if ([2,3,5,7].includes(n)){return true}
