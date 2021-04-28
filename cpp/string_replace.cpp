e3640d29ed2cd3dc45d7eee2357ffa7d8c304e99
akar-0 <68073164+akar-0@users.noreply.github.com>
2021-04-28 17:58:14 +0200

    Update string_replace.cpp

--- cpp/string_replace.cpp
+++ cpp/string_replace.cpp
@@ -1,2 +1,3 @@
 // https://www.codewars.com/kata/57a0885cbb9944e24c00008e/solutions/cpp
 // https://www.codewars.com/kata/57eae65a4321032ce000002d/solutions/cpp
+// https://www.codewars.com/kata/57eae20f5500ad98e50002c5/solutions
