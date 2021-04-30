3876be6534cbb53bb5ad4b9034e080c24623de0b
akar-0 <68073164+akar-0@users.noreply.github.com>
2021-04-30 15:09:35 +0200

    Update string_to_int.rs

--- rust/string_to_int.rs
+++ rust/string_to_int.rs
@@ -1,3 +1,7 @@
 // https://www.codewars.com/kata/554b4ac871d6813a03000035/solutions
 // https://www.codewars.com/kata/544675c6f971f7399a000e79/solutions/rust
 s.parse::<u64>().unwrap()
+
+use std::iter::FromIterator;
+
+String::from_iter(vector).parse::<u64>().unwrap()
