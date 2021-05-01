775e46d05fb4a19008ba7a03b45bf92e0a52b36c
akar-0 <68073164+akar-0@users.noreply.github.com>
2021-05-01 13:59:57 +0200

    Update case.rs

--- rust/case.rs
+++ rust/case.rs
@@ -1,2 +1,5 @@
 // https://www.codewars.com/kata/56efc695740d30f963000557/solutions/rust
 // https://www.codewars.com/kata/54b42f9314d9229fd6000d9c/solutions
+
+
+let s=String::from(w).to_lowercase();
