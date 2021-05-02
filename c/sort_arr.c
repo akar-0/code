44544e6d9b0b68d059e92162e405eba126eaaf91
akar-0 <68073164+akar-0@users.noreply.github.com>
2021-05-02 13:19:18 +0200

    Update sort_arr.c

--- c/sort_arr.c
+++ c/sort_arr.c
@@ -1,6 +1,9 @@
 // https://www.codewars.com/kata/5f55ecd770692e001484af7d/solutions/c
 // https://www.codewars.com/kata/550498447451fbbd7600041c/solutions
 // !!!! https://www.codewars.com/kata/5629db57620258aa9d000014/solutions/c
+
+#include <stdlib.h>
+
 int comp(const void * a, const void * b) {
    return (*(int*)a - *(int*)b);
 }
