83decd27d67844b68d4c2fd70ec56c7d610b14cb
akar-0 <68073164+akar-0@users.noreply.github.com>
2021-05-03 19:35:54 +0200

    Update regex.cpp

--- cpp/regex.cpp
+++ cpp/regex.cpp
@@ -1,3 +1,4 @@
 //https://www.codewars.com/kata/53921994d8f00b93df000bea/solutions
 // https://www.codewars.com/kata/reviews/5f647994e6d0d800010e0d5b/groups/5f686ca6e6d0d80001187afb
 // fnmatch?
+// https://www.codewars.com/kata/5b8be3ae36332f341e00015e/solutions/cpp
