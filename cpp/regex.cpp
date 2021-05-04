7b4e69f1a5a48949c5616019d8d12e20018f5e7e
akar-0 <68073164+akar-0@users.noreply.github.com>
2021-05-04 17:46:46 +0200

    Update regex.cpp

--- cpp/regex.cpp
+++ cpp/regex.cpp
@@ -2,3 +2,4 @@
 // https://www.codewars.com/kata/reviews/5f647994e6d0d800010e0d5b/groups/5f686ca6e6d0d80001187afb
 // fnmatch?
 // https://www.codewars.com/kata/5b8be3ae36332f341e00015e/solutions/cpp
+// https://www.codewars.com/kata/5b37a50642b27ebf2e000010/solutions
