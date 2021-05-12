9513067ae5e979547183472634a246155e4f810b
akar-0 <68073164+akar-0@users.noreply.github.com>
2021-05-12 19:55:17 +0200

    Update custom_sort.cpp

--- cpp/custom_sort.cpp
+++ cpp/custom_sort.cpp
@@ -1,3 +1,4 @@
 // https://www.codewars.com/kata/57ea5b0b75ae11d1e800006c/solutions/cpp
 //sort array of strings by length
       sort(array.begin(), array.end(),  [](string const &a, string const &b) {return a.length() < b.length();});
+// https://www.codewars.com/kata/5a8d2bf60025e9163c0000bc/solutions/cpp
