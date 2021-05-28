5e9d01b173aba9645b5c84640639ebb4929907b3
akar-0 <68073164+akar-0@users.noreply.github.com>
2021-05-29 00:05:32 +0200

    Update max_arr.rs

--- rust/max_arr.rs
+++ rust/max_arr.rs
@@ -1,2 +1,4 @@
 arr.iter().max().unwrap();
+arr.iter().minmax().into_option().map_or(0, blablabla)
 // https://www.codewars.com/kata/588a3c3ef0fbc9c8e1000095/solutions/rust
+// https://www.codewars.com/kata/559cc2d2b802a5c94700000c/solutions/rust
