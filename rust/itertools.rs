187c3cef28e5dceb548e2aa41ddb5d7bfa70dac1
akar-0 <68073164+akar-0@users.noreply.github.com>
2021-05-28 17:16:27 +0200

    Update itertools.rs

--- rust/itertools.rs
+++ rust/itertools.rs
@@ -1,4 +1,5 @@
 // https://www.codewars.com/kata/58c5577d61aefcf3ff000081/solutions/rust
+//https://www.codewars.com/kata/5aa1bcda373c2eb596000112/solutions/rust
 use itertools::Itertools;
 
 s.chars().sorted().rev().collect::<String>()
