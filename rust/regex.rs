e6239e27cc7203c84c08824614634be2467e3272
akar-0 <68073164+akar-0@users.noreply.github.com>
2021-05-29 16:38:08 +0200

    Update regex.rs

--- rust/regex.rs
+++ rust/regex.rs
@@ -2,3 +2,5 @@ use regex::Regex;
 // https://docs.rs/regex/1.1.0/regex/struct.Regex.html#method.replace_all
 // backrefs:
 // https://www.codewars.com/kata/5b049d57de4c7f6a6c0001d7/solutions/rust
+
+// https://www.codewars.com/kata/5a420163b6cfd7cde5000077/solutions/rust
