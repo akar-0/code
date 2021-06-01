f0a7520c1ab6683e31045a5293bb426284ad4c27
akar-0 <68073164+akar-0@users.noreply.github.com>
2021-06-01 18:44:10 +0200

    Update regex.rs

--- rust/regex.rs
+++ rust/regex.rs
@@ -4,3 +4,5 @@ use regex::Regex;
 // https://www.codewars.com/kata/5b049d57de4c7f6a6c0001d7/solutions/rust
 
 // https://www.codewars.com/kata/5a420163b6cfd7cde5000077/solutions/rust
+// pseudo back refs
+// https://www.codewars.com/kata/58708934a44cfccca60000c4/solutions/rust
