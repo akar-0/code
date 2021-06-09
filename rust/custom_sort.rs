4d05778a04bca430d0d5f38195e757a02b4a0cd9
akar-0 <68073164+akar-0@users.noreply.github.com>
2021-06-09 18:03:27 +0200

    Update custom_sort.rs

--- rust/custom_sort.rs
+++ rust/custom_sort.rs
@@ -1,3 +1,7 @@
 // https://www.codewars.com/kata/5629db57620258aa9d000014/solutions/rust
 // https://www.codewars.com/kata/5a8d2bf60025e9163c0000bc/solutions/rust
 // https://www.codewars.com/kata/55c6126177c9441a570000cc/solutions/rust
+//itertools:
+
+use itertools::Itertools;
+.sorted_by(...
