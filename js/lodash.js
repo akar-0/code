8a4af4303ce3e164f687400b53677f63e5d25a16
akar-0 <68073164+akar-0@users.noreply.github.com>
2021-06-12 13:40:30 +0200

    Update lodash.js

--- js/lodash.js
+++ js/lodash.js
@@ -1,3 +1,4 @@
 // https://www.codewars.com/kata/reviews/58b97873c1676274f300075b/groups/58cba52c256836a13500153f
 // https://www.codewars.com/kata/reviews/5a0bcf974be7159b480000d5/groups/5a0bfaf2591c894ea80005c2
 // https://www.codewars.com/kata/reviews/53c2c3e88d298dddda000462/groups/55fb5370fbf709bd680000d7
+const _=require('lodash')
