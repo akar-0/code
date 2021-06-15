b3f21b223cecacb07c763eada8e3f6598d4c1a65
akar-0 <68073164+akar-0@users.noreply.github.com>
2021-06-15 15:48:28 +0200

    Update permutations.js

--- js/permutations.js
+++ js/permutations.js
@@ -2,6 +2,7 @@
 // https://stackoverflow.com/questions/9960908/permutations-in-javascript
 // https://www.codewars.com/kata/5865a407b359c45982000036/solutions/javascript
 // https://www.codewars.com/kata/5254ca2719453dcc0b00027d/solutions
+// https://www.codewars.com/kata/56b18992240660a97c00000a/solutions/javascript
 function permutations (arr,p=[],l=arr.length) {
   if (l==1) {p.push(arr.slice())}
   for (let i=0;i<l;i++) {
