d0e76173ef2f3859cb2fc77730db0234f59f2f27
akar-0 <68073164+akar-0@users.noreply.github.com>
2021-06-21 10:11:40 +0200

    Update dec_to_bin.cpp

--- cpp/dec_to_bin.cpp
+++ cpp/dec_to_bin.cpp
@@ -3,3 +3,4 @@ std::string decToBin(int n){
 }
 // https://www.codewars.com/kata/551f37452ff852b7bd000139/solutions/cpp !!!!
 // https://www.codewars.com/kata/5959ec605595565f5c00002b/solutions/cpp
+//https://www.codewars.com/kata/5f3afc40b24f090028233490/solutions/cpp
