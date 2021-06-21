4361af7b31b7d159c547a145ed17f238ebd8eedc
akar-0 <68073164+akar-0@users.noreply.github.com>
2021-06-21 10:29:14 +0200

    Update dec_to_bin.cpp

--- cpp/dec_to_bin.cpp
+++ cpp/dec_to_bin.cpp
@@ -1,6 +1,7 @@
 std::string decToBin(int n){
   return n==0?"0":n==1?"1":decToBin(n>>1)+std::to_string(n&1);
 }
+bitset<32>(n).to_string();
 // https://www.codewars.com/kata/551f37452ff852b7bd000139/solutions/cpp !!!!
 // https://www.codewars.com/kata/5959ec605595565f5c00002b/solutions/cpp
 //https://www.codewars.com/kata/5f3afc40b24f090028233490/solutions/cpp
