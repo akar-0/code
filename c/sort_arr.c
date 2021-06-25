b3a962f63c33e8f6a2a70b5ac6e5b82fa78f6299
akar-0 <68073164+akar-0@users.noreply.github.com>
2021-06-25 22:56:09 +0200

    Update sort_arr.c

--- c/sort_arr.c
+++ c/sort_arr.c
@@ -2,20 +2,6 @@
 // https://www.codewars.com/kata/550498447451fbbd7600041c/solutions
 // !!!! https://www.codewars.com/kata/5629db57620258aa9d000014/solutions/c
 //!!!!   https://www.codewars.com/kata/58e0cb3634a3027180000040/solutions/c
-// https://www.codewars.com/kata/5f885fa9f130ea00207c7dc8/train/javascript
-// https://stackoverflow.com/questions/20833295/how-can-i-match-overlapping-strings-with-regex
-def loneliest(s):
-    s = s.strip()
-    ind = {i:c for (i,c) in enumerate(s) if c.isalpha()}
-    d = {}
-    L = [-1]+ sorted(ind)+[max(ind)+1]
-    for n in range(1,len(L)-1):
-        if L[n+1]-L[n-1]-1 in d:
-            d[L[n+1]-L[n-1]-1].append(ind[L[n]])
-        else:
-            d[L[n+1]-L[n-1]-1] = [ind[L[n]]]
-    else:
-        return d[max(d)] 
         
           
 #include <stdlib.h>
