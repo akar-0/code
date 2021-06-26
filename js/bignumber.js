b0e0be5d20ebf97f0fd176d9fb73bbb8092dcf7e
akar-0 <68073164+akar-0@users.noreply.github.com>
2021-06-26 13:33:59 +0200

    Update bignumber.js

--- js/bignumber.js
+++ js/bignumber.js
@@ -1,3 +1,3 @@
 //https://www.codewars.com/kata/57d81d428462e10e63000205/solutions/javascript
-
+// https://www.codewars.com/kata/5514e5b77e6b2f38e0000ca9/solutions
 const BigNumber = require("bignumber.js");
