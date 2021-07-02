b8311c948f5d7e636ccd78a31b341e57f7358720
akar-0 <68073164+akar-0@users.noreply.github.com>
2021-07-02 22:05:09 +0200

    Update groupBy.cpp

--- cpp/groupBy.cpp
+++ cpp/groupBy.cpp
@@ -1,1 +1,18 @@
 // https://en.cppreference.com/w/cpp/algorithm/unique
+
+
+  vector<pair<char, size_t>> groupBy(string s){
+    vector<pair<char, size_t>> v;
+    size_t i=0, l=s.length();
+    while (i<l){
+      char n=s[i];
+      size_t c=1;
+      while (i+1<l && s[i+1]==n){
+        i++;
+        c++;
+      }
+      v.push_back(make_pair(n,c));
+      i++;
+    }
+    return v;
+  }
