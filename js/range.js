d6bdc8f1ae8d4c80105955e928f6c77a082f1b1d
akar-0 <68073164+akar-0@users.noreply.github.com>
2021-07-07 23:54:31 +0200

    Update range.js

--- js/range.js
+++ js/range.js
@@ -1,2 +1,2 @@
 Array.from(range(min, max))
-function *rg(a, b, step=1) {for (var i = Math.min(a,b); i < Math.max(a,b); i+=step) yield i}
+function *range(a, b, step=1) {for (var i = Math.min(a,b); i < Math.max(a,b); i+=step) yield i}
