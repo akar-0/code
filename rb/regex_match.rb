f16a75b2165933d54dfe5e4b0acc2f9a60f9f6ea
akar-0 <68073164+akar-0@users.noreply.github.com>
2021-07-10 00:01:03 +0200

    Update regex_match.rb

--- rb/regex_match.rb
+++ rb/regex_match.rb
@@ -1,4 +1,6 @@
 // https://www.codewars.com/kata/5967a67c8b0fc0093e000062/solutions/ruby
 // https://www.codewars.com/kata/582e0450fe38013dbc0002d3/solutions/ruby
+// https://www.codewars.com/kata/545af88285166a44e700123a/solutions/ruby
+
 
  /regex/.match(s) != nil
