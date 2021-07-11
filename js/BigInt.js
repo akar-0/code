aa9d721a38916d5d0d39f1bb5f2e194b3d1cde75
akar-0 <68073164+akar-0@users.noreply.github.com>
2021-07-11 03:01:35 +0200

    Update BigInt.js

--- js/BigInt.js
+++ js/BigInt.js
@@ -1,2 +1,4 @@
+// https://www.codewars.com/kata/59bf943cafcda28e31000130/solutions
 const {BigInteger} = require('bignumber');
+const BigNumber = require('bignumber.js'); // ???
 new BigInteger(a).multiply(new BigInteger(b))
