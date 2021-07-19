3f7e16b5959d3841cdd1287c0435a273fd4d9f35
akar-0 <68073164+akar-0@users.noreply.github.com>
2021-07-20 00:57:29 +0200

    Update combinations.js

--- js/combinations.js
+++ js/combinations.js
@@ -1,7 +1,7 @@
 // https://www.codewars.com/kata/55e7280b40e1c4a06d0000aa/solutions/javascript
 // https://www.codewars.com/kata/58ad29bc4b852b14a4000050/solutions/javascript
 function comb(arr,n){
-  if (n==1) return arr
+  if (n==1) return arr.map(x=>[x])
   const L=[]
   for (let i=0;i<arr.length-n+1;i++){
     for (const x of comb(arr.slice(i+1), n-1)){
