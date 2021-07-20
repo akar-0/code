ce04022b7fdca725cf0545be974b3afbca453cf3
akar-0 <68073164+akar-0@users.noreply.github.com>
2021-07-20 17:19:04 +0200

    Update permutations.js

--- js/permutations.js
+++ js/permutations.js
@@ -3,10 +3,10 @@
 // https://www.codewars.com/kata/5865a407b359c45982000036/solutions/javascript
 // https://www.codewars.com/kata/5254ca2719453dcc0b00027d/solutions
 // https://www.codewars.com/kata/56b18992240660a97c00000a/solutions/javascript
-function permutations (arr,p=[],l=arr.length) {
+function permutations (arr,l=arr.length,p=[]) {
   if (l==1) {p.push(arr.slice())}
   for (let i=0;i<l;i++) {
-    permutations(arr,p,l-1)
+    permutations(arr,l-1,p)
     l%2?[arr[0],arr[l-1]]=[arr[l-1],arr[0]]:[arr[i],arr[l-1]]=[arr[l-1],arr[i]]
   }
   return p
