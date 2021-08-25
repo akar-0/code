74073f04779dcb4095bdd4e5788867e4e3db939d
akar-0 <68073164+akar-0@users.noreply.github.com>
2021-08-25 19:33:23 +0200

    Update switch.hs

--- haskell/switch.hs
+++ haskell/switch.hs
@@ -1,1 +1,3 @@
 -- https://www.codewars.com/kata/reviews/5b1c274b73bf5909450005c4/groups/5b1c3c21978fd7e988000ac2
+
+-- https://www.codewars.com/kata/reviews/567504d600b6b9436a000010/groups/605401906946d5000155259c
