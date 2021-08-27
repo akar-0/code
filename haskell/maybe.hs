a14dd74de0b45bacce443df352f12f8cc74dd0d4
akar-0 <68073164+akar-0@users.noreply.github.com>
2021-08-28 00:19:40 +0200

    Update maybe.hs

--- haskell/maybe.hs
+++ haskell/maybe.hs
@@ -1,3 +1,5 @@
 -- https://www.codewars.com/kata/57f604a21bd4fe771b00009c/solutions/haskell
 -- fromMaybe
 -- https://www.codewars.com/kata/5983cba828b2f1fd55000114/solutions/haskell
+-- fromJust
+-- https://www.codewars.com/kata/57da675dfa96908b16000040/solutions/haskell
