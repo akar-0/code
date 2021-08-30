6681d3ff1a2ff2c5d1d5912b944b306495afb3b0
akar-0 <68073164+akar-0@users.noreply.github.com>
2021-08-30 15:23:43 +0200

    Update counter.hs

--- haskell/counter.hs
+++ haskell/counter.hs
@@ -1,2 +1,3 @@
 -- https://www.codewars.com/kata/57a6633153ba33189e000074/solutions/haskell
 -- https://www.codewars.com/kata/585a033e3a36cdc50a00011c/solutions/haskell
+-- https://www.codewars.com/kata/56582133c932d8239900002e/solutions/haskell
