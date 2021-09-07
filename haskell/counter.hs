0a6259faafea3fc3e330a43f7c05e9b4ba3c2428
akar-0 <68073164+akar-0@users.noreply.github.com>
2021-09-07 19:36:47 +0200

    Update counter.hs

--- haskell/counter.hs
+++ haskell/counter.hs
@@ -2,3 +2,10 @@
 -- https://www.codewars.com/kata/585a033e3a36cdc50a00011c/solutions/haskell
 -- https://www.codewars.com/kata/56582133c932d8239900002e/solutions/haskell
 -- https://www.codewars.com/kata/55d2aee99f30dbbf8b000001/solutions/haskell
+
+
+import qualified Data.HashMap.Strict as HMap
+
+counter = HMap.fromListWith (+) $ zip a (repeat 1)
+-- array of tuples
+c = map (\c -> (c, counter HMap.! c)) $ nub a
