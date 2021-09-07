4d4d31a82b7159eeee42486cc30970dd2cc0b8f7
akar-0 <68073164+akar-0@users.noreply.github.com>
2021-09-07 19:36:59 +0200

    Update counter.hs

--- haskell/counter.hs
+++ haskell/counter.hs
@@ -2,7 +2,7 @@
 -- https://www.codewars.com/kata/585a033e3a36cdc50a00011c/solutions/haskell
 -- https://www.codewars.com/kata/56582133c932d8239900002e/solutions/haskell
 -- https://www.codewars.com/kata/55d2aee99f30dbbf8b000001/solutions/haskell
-
+-- https://www.codewars.com/kata/57a6633153ba33189e000074/solutions/haskell
 
 import qualified Data.HashMap.Strict as HMap
 
