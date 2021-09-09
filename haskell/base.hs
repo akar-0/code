7e068e7dad165ddb3886686decb333cd922aad28
akar-0 <68073164+akar-0@users.noreply.github.com>
2021-09-09 08:58:31 +0200

    Update base.hs

--- haskell/base.hs
+++ haskell/base.hs
@@ -1,2 +1,3 @@
 -- showIntAtBase
 -- https://www.codewars.com/kata/reviews/5f23d177d7c4750001575193/groups/5f23d178d7c4750001575197
+-- https://www.codewars.com/kata/56be4affc5dc03b84b001d2d/solutions/haskell
