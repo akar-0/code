6c4ac0541310739be930f7ea711eabe968a01f29
akar-0 <68073164+akar-0@users.noreply.github.com>
2021-09-17 06:51:31 +0200

    Update reduce.hs

--- haskell/reduce.hs
+++ haskell/reduce.hs
@@ -1,3 +1,4 @@
 -- https://www.codewars.com/kata/54da5a58ea159efa38000836/solutions/haskell
 g = foldl (\acc curr -> f acc curr) 0
 -- https://www.codewars.com/kata/5872bb7faa04282110000124/solutions/haskell
+-- https://www.codewars.com/kata/5a4a2973d8e14586c700000a/solutions/haskell
