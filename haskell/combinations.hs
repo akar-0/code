648967378f6fc3e6843342085105befeb8c5f477
akar-0 <68073164+akar-0@users.noreply.github.com>
2021-09-21 15:34:52 +0200

    Update combinations.hs

--- haskell/combinations.hs
+++ haskell/combinations.hs
@@ -2,3 +2,4 @@
 [(x,y) | x <- xs, y <- ys]
 --combinations
 -- https://stackoverflow.com/questions/52602474/function-to-generate-the-unique-combinations-of-a-list-in-haskell/52605612
+-- https://www.codewars.com/kata/577e694af5db624cf30002d0/solutions/haskell
