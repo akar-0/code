5b4727c75019c04286bb6a440a717c4a38053137
akar-0 <68073164+akar-0@users.noreply.github.com>
2021-09-21 15:30:17 +0200

    Update combinations.hs

--- haskell/combinations.hs
+++ haskell/combinations.hs
@@ -1,2 +1,4 @@
 -- cartesian product
 [(x,y) | x <- xs, y <- ys]
+--combinations
+-- https://stackoverflow.com/questions/52602474/function-to-generate-the-unique-combinations-of-a-list-in-haskell/52605612
