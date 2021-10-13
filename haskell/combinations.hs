a114a02c26e557244172c253e4cce9a1547dcf53
akar-0 <68073164+akar-0@users.noreply.github.com>
2021-10-13 18:03:28 +0200

    Update combinations.hs

--- haskell/combinations.hs
+++ haskell/combinations.hs
@@ -2,6 +2,8 @@
 [(x,y) | x <- xs, y <- ys]
 prod a b = [[x,y] | x <- a, y <- b]
 
+combinations = (. subsequences) . filter . (. length) . (==)
+
 --combinations
 -- https://stackoverflow.com/questions/52602474/function-to-generate-the-unique-combinations-of-a-list-in-haskell/52605612
 -- https://www.codewars.com/kata/577e694af5db624cf30002d0/solutions/haskell
