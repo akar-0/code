5089d54c386387096dcb441334d30ff7ccbec629
akar-0 <68073164+akar-0@users.noreply.github.com>
2021-10-17 23:27:19 +0200

    Update counter.rs

--- rust/counter.rs
+++ rust/counter.rs
@@ -1,6 +1,6 @@
 // https://stackoverflow.com/questions/64178272/what-is-the-idiomatic-rust-way-to-build-a-hashmap-of-character-counts
 // https://www.codewars.com/kata/54bf1c2cd5b56cc47f0007a1/solutions/rust 
-
+// https://www.codewars.com/kata/585a033e3a36cdc50a00011c/solutions/rust !!!!!!!!!!!!
 use std::collections::HashMap;
 
     let mut a: HashMap<char, i32> = HashMap::new();
