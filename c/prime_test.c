10bd2560fccd996f0bc86196d9c80af23b6bbd14
akar-0 <68073164+akar-0@users.noreply.github.com>
2021-10-20 15:57:20 +0200

    Update prime_test.c

--- c/prime_test.c
+++ c/prime_test.c
@@ -20,3 +20,28 @@ bool is_prime(int n)
   }
    return true;
 }
+// gap in primes
+// https://www.codewars.com/kata/561e9c843a2ef5a40c0000a4/solutions/c
+int isPrime(long long n){
+  if (n<2) return 0;
+  if (n==2||n==3||n==5||n==7) return 1;
+  if (!(n%2)||!(n%3)||!(n%5)) return 0;
+  long long c[8]={4, 2, 4, 2, 4, 6, 2, 6};
+  long long p=7;
+  unsigned i=0;
+  while (p*p<=n){
+    if (!(n%p)) return 0;
+    p+=c[i];
+    i=(i+1)%8;
+  }
+  return 1;
+}
+long long nextPrime(long long n){
+  if (n<2) return 2;
+  if (!(n&1)) n++;
+  else n+=2;
+  while (1){
+    if (isPrime(n)) return n;
+    n+=2;
+  }
+}
