5f8cab3bdcc638a09f9ba9a589481882f9821585
akar-0 <68073164+akar-0@users.noreply.github.com>
2021-10-30 00:20:34 +0200

    Update counter.rb

--- rb/counter.rb
+++ rb/counter.rb
@@ -1,3 +1,5 @@
   count=Hash.new {|h, key| h[key]=0}
   a.each{|x| count[x]+=1}
 # https://www.codewars.com/kata/reviews/5865355c02698c58e4000175/groups/5a66da2374df2abfcb000b8a
+# crystal
+  count=Hash(Int32, Int32).new(0)
