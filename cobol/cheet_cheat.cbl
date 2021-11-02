21ffd1f555ec732b4d59e3af27be98ab0da7d4d8
akar-0 <68073164+akar-0@users.noreply.github.com>
2021-11-02 22:11:28 +0100

    Update cheet_cheat.cbl

--- cobol/cheet_cheat.cbl
+++ cobol/cheet_cheat.cbl
@@ -1,3 +1,4 @@
 // ifelse https://www.codewars.com/kata/574b3b1599d8f897470018f6/solutions/cobol
 // https://www.developpez.net/forums/d647072/autres-langages/autres-langages/cobol/z-os-fonctions-routines-service-enterprise-cobol-for-z-os/
 // https://www.codewars.com/kata/5ab6538b379d20ad880000ab/solutions/cobol
+// https://www.codewars.com/kata/523b623152af8a30c6000027/solutions/cobol
