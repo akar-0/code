9c7f9876dd3bc19860a46b717316aeca82c6ac06
akar-0 <68073164+akar-0@users.noreply.github.com>
2021-11-07 02:41:58 +0100

    Create popcount.cbl

---
+++ cobol/popcount.cbl
@@ -0,0 +1,9 @@
+      Set o to Zero. 
+      Set r to Zero.
+      set b to 2.
+      
+      Perform PopCount until n is equal to 0.
+       
+       PopCount.
+          Divide b into n giving n remainder r,
+          Add r to o.
