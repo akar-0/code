868bfbf910ba1f7ee88483253a32ef081d69bed7
akar-0 <68073164+akar-0@users.noreply.github.com>
2021-12-02 13:04:07 +0100

    Create gcd.cbl

---
+++ cobol/gcd.cbl
@@ -0,0 +1,18 @@
+       identification division.
+       program-id. gcd.
+       data division.
+       local-storage section.
+       01 c           pic 9(10).
+       linkage section.
+       01 a           pic 9(10).
+       01 b           pic 9(10).
+       01 result      pic 9(10).
+       procedure division using a b result.
+           perform until b = 0,
+              move b to c,
+              compute b = function rem(a, b),
+              move c to a,
+           end-perform
+           move a to result
+           goback.
+       end program gcd.
