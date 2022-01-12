2369ba2c3a7156dffa132d3a0973130eff9d4b5a
akar-0 <68073164+akar-0@users.noreply.github.com>
2022-01-12 14:46:31 +0100

    Update strings.cbl

--- cobol/new_tr/strings.cbl
+++ cobol/new_tr/strings.cbl
@@ -21,3 +21,78 @@
       
       
       
+
+      * tests
+       identification division.
+       program-id. tests.
+      
+       data division.
+       working-storage section.
+       01  s.
+           05 s-length     pic 9(2).
+           05 s-char       pic x occurs 0 to 30 times 
+                           depending on arr-length.
+       01  result.
+           05 res-length   pic 9(2).
+           05 res          pic x occurs 0 to 30 times 
+                           depending on res-length.
+       01  expected.
+           05 xp-length      pic 9(2).
+           05 e-chars.
+              07 xp          pic x occurs 0 to 30 times 
+                             depending on xp-length.
+       01  i usage index.
+       01  j usage index.
+       01  s1 constant ''.
+       01  e1 constant ''.
+       01  r-disp            pic z(10)9.
+       01  e-disp            pic z(10)9.
+      
+       procedure division.
+           testsuite 'Fixed tests'.
+           move length of s1 to s-length
+           move s1 to s-chars
+           move length of e1 to xp-length
+           move e1 to e-chars
+           perform dotest
+      
+           testsuite 'Random tests'.
+           perform set-random-seed
+           perform 0 times
+              compute s-length = 100 * function random
+              move 0 to xp-length
+              perform varying i from 1 until i > s-length
+              end-perform
+              perform dotest
+            end-perform
+           end tests.
+      
+       dotest.
+           if s-length > 86
+             move s-length to r-disp
+             testcase 'Testing string of length ' function trim(r-disp).
+            else 
+             testcase 'Testing s = "' function trim(s-chars) '"'.
+            end-if
+           
+           initialize result
+           call 'NAME' using 
+               by content s
+               by reference result
+      
+           if res-length <> xp-length
+                move res-length to r-disp
+                move xp-length to e-disp
+                initialize assertion-message
+                string 'Incorrect length of result: expected '
+                        function trim(e-disp) ', actual '
+                        function trim(r-disp)
+                into assertion-message
+                perform assert-false
+           else
+                expect res-chars to be e-chars.
+           end-if
+           .
+      
+       end program tests.
+   
