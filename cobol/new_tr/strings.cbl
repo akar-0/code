a50ba5c8240c9be36dddf4c7396a895b15703427
akar-0 <68073164+akar-0@users.noreply.github.com>
2022-01-12 14:37:23 +0100

    Update strings.cbl

--- cobol/new_tr/strings.cbl
+++ cobol/new_tr/strings.cbl
@@ -8,9 +8,9 @@
            05 s-char       pic x occurs 0 to 20 times 
                                   depending on arr-length.
        01  result.
-           05 res-length     pic 9(2).
-           05 res            pic x occurs 0 to 20 times 
-                                   depending on res-length.
+           05 res-length   pic 9(2).
+           05 res          pic x occurs 0 to 20 times 
+                                 depending on res-length.
       
        procedure division using s result.
       
