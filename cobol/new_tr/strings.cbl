67c491717ed31a9851dc9319e78af7fa111d017b
akar-0 <68073164+akar-0@users.noreply.github.com>
2022-01-13 21:25:11 +0100

    Update strings.cbl

--- cobol/new_tr/strings.cbl
+++ cobol/new_tr/strings.cbl
@@ -8,7 +8,7 @@
        01  s.
            05 s-length     pic 9(2).
            05 s-char       pic x occurs 0 to 20 times 
-                                  depending on arr-length.
+                                  depending on s-length.
        01  result.
            05 res-length   pic 9(2).
            05 res          pic x occurs 0 to 20 times 
