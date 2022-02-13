fd4b20c5e6c6dfb77c91836b14232ed84fb74553
akar-0 <68073164+akar-0@users.noreply.github.com>
2022-02-13 12:15:21 +0100

    Update strings.cbl

--- cobol/new_tr/strings.cbl
+++ cobol/new_tr/strings.cbl
@@ -6,13 +6,13 @@
       
        linkage section.
        01  s.
-           05 s-length     pic 9(2).
-           05 s-char       pic x occurs 0 to 20 times 
-                                  depending on s-length.
+           05 sLen         pic 9(2).
+           05 sChar        pic x occurs 0 to 20 times 
+                                  depending on sLen.
        01  result.
-           05 res-length   pic 9(2).
+           05 resLen       pic 9(2).
            05 res          pic x occurs 0 to 20 times 
-                                 depending on res-length.
+                                 depending on resLen.
       
        procedure division using s result.
       
@@ -31,20 +31,20 @@
        data division.
        working-storage section.
        01  s.
-           05  s-length      pic 9(2).
-           05  s-chars.
-               07 s-char     pic x occurs 0 to 30 times 
-                             depending on s-length.
+           05  sLen         pic 9(2).
+           05  sChars.
+              07  sChar     pic x occurs 0 to 20 times 
+                                  depending on sLen.
        01  result.
-           05  res-length    pic 9(2).
-           05  res-chars.
-              07 res         pic x occurs 0 to 30 times 
-                             depending on res-length.
+           05  resLen       pic 9(2).
+           05  resChars.
+               07 resChar   pic x occurs 0 to 20 times 
+                                  depending on resLen.
        01  expected.
-           05  xp-length     pic 9(2).
-           05  e-chars.
-              07  xp         pic x occurs 0 to 30 times 
-                             depending on xp-length.
+           05  eLen         pic 9(2).
+           05  eChars.
+               07 eChar     pic x occurs 0 to 20 times 
+                                 depending on eLen.
        01  i usage index.
        01  j usage index.
        01  s1 constant ''.
@@ -64,42 +64,42 @@
       
        procedure division.
            testsuite 'Fixed tests'.
-           move length of s1 to s-length
-           move s1 to s-chars
-           move length of e1 to xp-length
-           move e1 to e-chars
+           move length of s1 to sLen
+           move s1 to eChars
+           move length of e1 to eLen
+           move e1 to eChars
            perform dotest
            
       
            testsuite 'Random tests'.
            perform set-random-seed
            perform 0 times
-              compute s-length = 100 * function random
-              move 0 to xp-length
-              perform varying i from 1 until i > s-length
+              compute eLen = 100 * function random
+              move 0 to eLen
+              perform varying i from 1 until i > sLen
                 move function char(33 + function integer
                                   (95 * function random))
-                     to s-char(i)
+                     to sChar(i)
               end-perform
               perform dotest
             end-perform
            end tests.
       
        dotest.
-           if s-length > 86
-             move s-length to r-disp
+           if sLen > 86
+             move sLen to r-disp
              testcase 'Testing string of length ' function trim(r-disp).
             else 
-             testcase 'Testing s = "' s-chars '"'.
+             testcase 'Testing s = "' sChars '"'.
             end-if
            
            call 'NAME' using 
                by content s
                by reference result
       
-           if res-length <> xp-length
-                move res-length to r-disp
-                move xp-length to e-disp
+           if resLen <> eLen
+                move resLen to r-disp
+                move eLen to e-disp
                 initialize assertion-message
                 string 'Incorrect length of result: expected '
                         function trim(e-disp) ', actual '
@@ -107,7 +107,7 @@
                 into assertion-message
                 perform assert-false
            else
-                expect res-chars to be e-chars.
+                expect resChars to be eChars.
            end-if
            .
       
