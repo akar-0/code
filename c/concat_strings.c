06f47bcc15f431a3eaf89f4d7f4bd98734f7bd1e
akar-0 <68073164+akar-0@users.noreply.github.com>
2022-04-16 11:39:23 +0200

    Update concat_strings.c

--- c/concat_strings.c
+++ c/concat_strings.c
@@ -1,1 +1,4 @@
 // https://www.codewars.com/kata/53dc23c68a0c93699800041d/solutions/c
+// https://www.codewars.com/kumite/62596cfd43cbf600160473d9?sel=62596cfd43cbf600160473d9
+
+ptr += sprintf(ptr, "some format %s %d", "abcd", 8);
