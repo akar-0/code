248e1d7ee221cb8c9f57ade52b6f253d4a2808fb
akar-0 <68073164+akar-0@users.noreply.github.com>
2022-05-07 21:40:29 +0200

    Update counter.rb

--- rb/counter.rb
+++ rb/counter.rb
@@ -1,3 +1,4 @@
+# https://www.codewars.com/kata/reviews/53b7cad52a21f4894f000219/groups/53bac685392478bf030001c5            
   count=Hash.new {|h, key| h[key]=0}
   a.each{|x| count[x]+=1}
 # https://www.codewars.com/kata/reviews/5865355c02698c58e4000175/groups/5a66da2374df2abfcb000b8a
