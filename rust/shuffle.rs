afe958e19570569d36c7d261ed8c57d064b428d4
akar-0 <68073164+akar-0@users.noreply.github.com>
2022-05-20 23:28:02 +0200

    Update shuffle.rs

--- rust/shuffle.rs
+++ rust/shuffle.rs
@@ -1,1 +1,10 @@
 // https://www.codewars.com/kumite/6283eb3084bbe5005e73ab9b?sel=6283eb3084bbe5005e73ab9b
+
+
+    use rand::seq::SliceRandom;
+
+
+
+        let mut rng = thread_rng();
+        let mut xs = (0..500).collect::<Vec<usize>>();
+        xs.shuffle(&mut rng);
