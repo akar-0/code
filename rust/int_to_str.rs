9b7c66eccdebad6b003b4c96d22f290a76d158a0
akar-0 <68073164+akar-0@users.noreply.github.com>
2022-05-25 13:41:41 +0200

    Update int_to_str.rs

--- rust/int_to_str.rs
+++ rust/int_to_str.rs
@@ -1,2 +1,16 @@
 // https://www.codewars.com/kata/5265326f5fda8eb1160004c8/solutions/rust
 n.to_string()
+
+
+// https://www.codewars.com/kumite/628dcc0645fef38508c663b7?sel=628dcc0645fef38508c663b7
+
+
+
+use std::char;
+
+
+
+c.to_digit(10).unwrap()
+
+
+char::from_digit(r, 10).unwrap()
