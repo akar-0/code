dd2afa92f295ab9c5b64cf710d4bd8cfe403358a
akar-0 <68073164+akar-0@users.noreply.github.com>
2022-05-25 21:09:01 +0200

    Update big_int.rs

--- rust/big_int.rs
+++ rust/big_int.rs
@@ -6,3 +6,4 @@ use num::traits::One;
 
 // https://www.codewars.com/kata/559b8e46fa060b2c6a0000bf/solutions/rust
 // https://www.codewars.com/kumite/62876ebd0b56c3002576c0bb?sel=62876ebd0b56c3002576c0bb
+// https://www.codewars.com/kumite/628e6f1e2324192c65cd8c98?sel=628e6f1e2324192c65cd8c98
