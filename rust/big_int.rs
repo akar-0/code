4c653e1688ede2ffdd849292d8907c0c585ebffb
akar-0 <68073164+akar-0@users.noreply.github.com>
2022-05-26 10:17:23 +0200

    Update big_int.rs

--- rust/big_int.rs
+++ rust/big_int.rs
@@ -3,6 +3,13 @@ extern crate num;
 use num::bigint::BigInt;
 use num::bigint::ToBigInt;
 use num::traits::One;
+use num::traits::Zero;
+use std::str::FromStr;
+
+
+BigInt::from_str(...).unwrap()
+
+
 
 // https://www.codewars.com/kata/559b8e46fa060b2c6a0000bf/solutions/rust
 // https://www.codewars.com/kumite/62876ebd0b56c3002576c0bb?sel=62876ebd0b56c3002576c0bb
