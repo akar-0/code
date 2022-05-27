05db21ff450245962d3fceccef58eac2653be306
akar-0 <68073164+akar-0@users.noreply.github.com>
2022-05-27 15:03:24 +0200

    Update big_int.rs

--- rust/big_int.rs
+++ rust/big_int.rs
@@ -14,3 +14,4 @@ BigInt::from_str(...).unwrap()
 // https://www.codewars.com/kata/559b8e46fa060b2c6a0000bf/solutions/rust
 // https://www.codewars.com/kumite/62876ebd0b56c3002576c0bb?sel=62876ebd0b56c3002576c0bb
 // https://www.codewars.com/kumite/628e765f513d4f0049e6bb7b?sel=628e765f513d4f0049e6bb7b
+// https://www.codewars.com/kumite/6290c93158d87b00308c8e74?sel=6290c93158d87b00308c8e74
