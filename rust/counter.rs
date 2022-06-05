3bfa5b56bc09a1d0fd0a956c9c24157393539508
akar-0 <68073164+akar-0@users.noreply.github.com>
2022-06-05 22:15:22 +0200

    Update counter.rs

--- rust/counter.rs
+++ rust/counter.rs
@@ -1,6 +1,7 @@
 // https://stackoverflow.com/questions/64178272/what-is-the-idiomatic-rust-way-to-build-a-hashmap-of-character-counts
 // https://www.codewars.com/kata/54bf1c2cd5b56cc47f0007a1/solutions/rust 
 // https://www.codewars.com/kata/585a033e3a36cdc50a00011c/solutions/rust !!!!!!!!!!!!
+// cf alphabetic anagrams
 use std::collections::HashMap;
 
     let mut a: HashMap<char, i32> = HashMap::new();
