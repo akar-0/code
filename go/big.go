a5432d8243a0c47105d29146287c3d27d47491a4
akar-0 <68073164+akar-0@users.noreply.github.com>
2022-06-11 15:09:13 +0200

    Update big.go

--- go/big.go
+++ go/big.go
@@ -1,2 +1,4 @@
 // https://www.codewars.com/kata/54cb771c9b30e8b5250011d4/solutions
 // https://www.codewars.com/kumite/629fbb01783f130ece282c3f?sel=62a032094e2c2441e9d0d5bb
+// lcm
+// https://www.codewars.com/kumite/62a48f3e3abc8d377f965c1b?sel=62a48f3e3abc8d377f965c1b
