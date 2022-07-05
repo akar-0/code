e138f660107da5e782ed0e2177490be54a665573
akar-0 <68073164+akar-0@users.noreply.github.com>
2022-07-05 15:03:56 +0200

    Update regex.rs

--- rust/regex.rs
+++ rust/regex.rs
@@ -1,4 +1,9 @@
+use once_cell::sync::Lazy;
 use regex::Regex;
+
+fn ... {
+    static RE: Lazy<Regex> = Lazy::new(|| Regex::new("REGEX").unwrap());
+}
 // https://docs.rs/regex/1.1.0/regex/struct.Regex.html#method.replace_all
 // backrefs:
 // https://www.codewars.com/kata/5b049d57de4c7f6a6c0001d7/solutions/rust
