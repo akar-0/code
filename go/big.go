36f718a90419b9249edf4b5f9346a1b4d4885b7b
akar-0 <68073164+akar-0@users.noreply.github.com>
2022-07-13 10:51:30 +0200

    Update big.go

--- go/big.go
+++ go/big.go
@@ -2,3 +2,8 @@
 // https://www.codewars.com/kumite/629fbb01783f130ece282c3f?sel=62a032094e2c2441e9d0d5bb
 // lcm
 // https://www.codewars.com/kumite/62a48f3e3abc8d377f965c1b?sel=62a48f3e3abc8d377f965c1b
+
+
+
+n := new(big.Int)
+n.SetString("833465465474928364", 10)
