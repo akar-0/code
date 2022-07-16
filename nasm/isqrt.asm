9a6eec13298901df02c0f9a22c818caebd995bd8
akar-0 <68073164+akar-0@users.noreply.github.com>
2022-07-16 16:47:26 +0200

    Create isqrt.asm

---
+++ nasm/isqrt.asm
@@ -0,0 +1,40 @@
+;    int isqrt(int n) ;
+;    n := edi
+
+
+global isqrt
+
+section .text
+
+isqrt:
+    cmp edi, 0
+    jl _retFalse
+    cmp edi, 3
+    jl _retTrivial
+    ;  compute isqrt(n)
+    ;  hardware algorithm (from original C code in `Hacker's delight` by H.S Wawrren Jr., Addison Wesley, 2013, 2nd ed., p. 286)
+    mov eax, edi           ; x
+    xor ecx, ecx           ; y   (isqrt at the end of the algorithm)
+    mov edx, 0x40000000    ; m
+_loop:
+    test edx, edx          ; until m = 0
+    jz _endisqrt
+    mov r8d, ecx           ; b = y
+    or r8d, edx            ; b = y | m
+    shr ecx, 1             ; y >>= 1
+    cmp eax, r8d           ; if x >= b
+    jl _next
+    sub eax, r8d           ; x -= b
+    or ecx, edx            ; y |= m
+_next:
+    shr edx, 2             ; m >>= 2
+    jmp _loop
+_endisqrt:
+    mov eax, ecx
+    ret
+_retFalse:
+    mov eax, -1
+    ret
+_retTrivial:
+    mov eax, edi
+    ret
