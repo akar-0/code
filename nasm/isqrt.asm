44bd5d0ff0cd6aeb685cdf9f895cb9ca09f8f8a9
akar-0 <68073164+akar-0@users.noreply.github.com>
2022-07-20 11:03:45 +0200

    Update isqrt.asm

--- nasm/isqrt.asm
+++ nasm/isqrt.asm
@@ -38,3 +38,39 @@ _retFalse:
 _retTrivial:
     mov eax, edi
     ret
+
+
+; --------------------------------------------------------------------
+
+section .text
+
+isqrt:  
+    cmp rdi, 3
+    jl .retTrivial
+    ;  compute isqrt(n)
+    ;  hardware algorithm (from original C code in `Hacker's delight` by H.S Wawrren Jr., Addison Wesley, 2013, 2nd ed., p. 286)
+    mov rax, rdi           ; x
+    xor rcx, rcx           ; y   (isqrt at the end of the algorithm)
+    mov rdx, 0x40000000    ; m
+.loop:
+    test rdx, rdx          ; until m = 0
+    jz .endisqrt
+    mov r8, rcx           ; b = y
+    or r8, rdx            ; b = y | m
+    shr rcx, 1             ; y >>= 1
+    cmp rax, r8           ; if x >= b
+    jl .next
+    sub rax, r8           ; x -= b
+    or rcx, rdx            ; y |= m
+.next:
+    shr rdx, 2             ; m >>= 2
+    jmp .loop
+.endisqrt:
+    mov rax, rcx
+    ret
+.retFalse:
+    mov rax, -1
+    ret
+.retTrivial:
+    mov rax, rdi
+    ret
