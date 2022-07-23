8c61d48133c7c215ae398c4c406553a5ad428459
akar-0 <68073164+akar-0@users.noreply.github.com>
2022-07-23 17:15:09 +0200

    Update regex.rs

--- rust/regex.rs
+++ rust/regex.rs
@@ -15,3 +15,15 @@ fn ... {
 // https://www.codewars.com/kumite/6298512bd747b9002f4eb5dd?sel=6298512bd747b9002f4eb5dd
 // named groups
 // https://www.codewars.com/kumite/629a327dbd528d39da601a04?sel=629a327dbd528d39da601a04
+
+
+// Unnamed...
+use fancy_regex::Regex;
+use once_cell::sync::OnceCell;
+
+fn f(x: &[[char; 8]; 8]) -> bool {
+    static S_RE: &str = "[♛♜] *♔|♔ *[♛♜]|[♛♜](.{8} )*.{8}♔|♔(.{8} )*.{8}[♛♜]|[♛♝]((?!n).{9} )*(?!n).{9}♔|[♛♝]((?<!n.).{7} )*(?<!n).{7}♔|♔((?!n).{9} )*(?!n).{9}[♛♝]|♔((?<!n.).{7} )*(?<!n).{7}[♛♝]|♟(?!n).{9}♔|♟(?<!n.).{7}♔|♞(?!n).{18}♔|♞(?<!n.).{16}♔|♞(?!n|.n).{10}♔|♞(?<!n.|n..)(?![^n]{6}).{6}♔|♔(?<!n.|n..)(?![^n]{6}).{6}♞|♔(?!n|.n).{10}♞|♔(?<!n.).{16}♞|♔(?!n).{18}♞";
+    static RE: OnceCell<Regex> = OnceCell::new();
+    let s: String = x.join(&'n').into_iter().collect();
+    RE.get_or_init(|| Regex::new(S_RE).unwrap()).is_match(&s).unwrap()
+}
