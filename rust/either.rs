08b1b0e73e6c38c160e722ae7884505d7538cd5a
akar-0 <68073164+akar-0@users.noreply.github.com>
2022-07-28 08:41:17 +0200

    Update either.rs

--- rust/either.rs
+++ rust/either.rs
@@ -1,1 +1,2 @@
 // https://www.codewars.com/kumite/62d917c19d945c678e9513b0?sel=62d917c19d945c678e9513b0
+// https://www.codewars.com/kata/reviews/62e142bcd08f530001b9386a/groups/62e14c79d08f530001b93b2d
