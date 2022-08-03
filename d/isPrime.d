e1ebecc3d675a768b1ed3563ba446a74d45a0ff3
akar-0 <68073164+akar-0@users.noreply.github.com>
2022-08-03 15:19:17 +0200

    Update isPrime.d

--- d/isPrime.d
+++ d/isPrime.d
@@ -13,3 +13,48 @@ bool isPrime(long n)
   }
  return true;
 }
+
+
+
+
+// POWAAA
+
+import  std.range : chain, drop, enumerate, iota, only, padLeft, repeat, retro, stride, take, zip;
+import  std.algorithm.iteration : each, filter, fold, joiner, map, sum;
+import  std.math : sqrt;
+import  std.array : array;
+    import std.range;
+    import std.stdio: write, writeln, writef, writefln;
+    import std.algorithm.comparison : equal;
+
+static auto primes()
+{
+    // limit
+    auto n = 20_000;
+    auto c = (n%6)>1 ? 1 : 0;
+    auto a = [n, n - 1, n + 4, n + 3, n + 2, n + 1];
+    n = a[n%6];
+    auto L = (true).repeat(n/3).array;
+    L[0] = false;
+    auto sq = cast(uint) sqrt(cast(double) n);
+    for (auto i = 0 ; i <= sq / 3 ; i++)
+    {
+        if (L[i])
+        {
+            auto j=(3*i+1)|1;
+            auto h=j*j;
+            for (auto e = h / 3 ; e < L.length ; e += 2 *j)
+            {
+                L[e] = false;
+            }
+            for (auto e = (h+4*j-2*j*(i%2))/3 ; e < L.length ; e += 2 * j)
+            {
+                L[e] = false;
+            }
+        }
+    }
+    writeln(L.length);
+ //   writeln(L);
+    auto P = assumeSorted([2u,3].chain(iota(1, n/3-c).filter!(x => L[x]).map!(i => (3*i+1)|1)).array);
+    return P;
+}
