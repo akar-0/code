d5ee7656aae2a8f604004b528a67955afdb23772
akar-0 <68073164+akar-0@users.noreply.github.com>
2022-08-06 12:41:21 +0200

    Update bisect.rs

--- rust/bisect.rs
+++ rust/bisect.rs
@@ -12,3 +12,21 @@ fn bisect(m: f64, xs: &[f64]) -> usize {
     }
     lo
 }
+
+
+fn bisect(m: u32) -> usize {
+    let mut lo = 0;
+    let mut hi = L.len();
+    let mut mid;
+    while lo < hi {
+        mid = (lo + hi) / 2;
+        if m == L[mid] {
+            return mid;
+        } else if m < L[mid] {
+            hi = mid;
+        } else {
+            lo = mid + 1;
+        }
+    }
+    lo
+}
