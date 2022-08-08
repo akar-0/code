dd9a1f697fcc7669b01bd822a675f1cdb387180d
akar-0 <68073164+akar-0@users.noreply.github.com>
2022-08-08 17:29:46 +0200

    Update draft.py

--- py/draft.py
+++ py/draft.py
@@ -7,3 +7,18 @@ for a in count(2):
             s=sum(e)
             L.append([tuple(e), sum(e)])
     if s>1_500_000:break
+
+        
+        
+        
+from collections import defaultdict
+d=defaultdict(list)
+for n in count(2):
+    for m in range(n+1,2*n):
+        a=n*n
+        b=m*n
+        c=m**2-n**2
+        p=a+b+c
+        d[p].append((a,b,c))
+        if p > 1500_000:break
+    
