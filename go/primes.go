7862bce722f1c153c032008091cc5ed8524895de
akar-0 <68073164+akar-0@users.noreply.github.com>
2022-08-10 19:28:46 +0200

    Update primes.go

--- go/primes.go
+++ go/primes.go
@@ -73,3 +73,73 @@ import "math/big"
 func isPrime(n int) bool { return big.NewInt(int64(n)).ProbablyPrime(0) }
 
 
+
+package kata
+
+
+
+import (
+  "math"
+)
+
+var primes = []int{2,3}
+var h = make(map[int]bool)
+
+func makePrimes() {
+  if len(primes) > 2 { return }
+  n := 30000
+  var c int
+  if (n % 6) > 1 { c = 1 }
+  n = []int{n, n - 1, n + 4, n + 3, n + 2, n + 1}[n%6]
+  ls := make([]bool, n/3)
+  
+  for i := 1 ; i < n / 3 ; i++ {
+    ls[i] = true
+  }
+  sq := int(math.Sqrt(float64(n)))
+  for i := 0 ; i <= sq / 3 ; i++ {
+    if ls[i] {
+      j := (3*i+1)|1
+      h := j * j
+      for e := h / 3 ; e < len(ls) ; e += 2 * j {
+        ls[e] = false
+      }
+      for e := (h+4*j-2*j*(i%2))/3 ; e < len(ls) ; e += 2 * j {
+        ls[e] = false
+      }
+    }
+  }
+  for i := 1 ; i < n/3 - c ; i++ {
+    if ls[i] {
+      primes = append(primes, (3*i+1)|1)
+    }
+  }
+  for _,p := range primes { h[p] = true }
+}
+
+func BigPrimefacDiv(n int) []int {
+  makePrimes()
+  if n < 0 { n = -n }
+  if h[n] { return nil }
+  var i, x, y int
+  var flag bool
+  p := primes[0]
+  for p * p <= n {
+    p = primes[i]
+    i++
+    if n % p == 0 {
+      y = p
+      if ! flag {
+        n /= p
+        x = n
+        flag = true
+      }
+      for n % p == 0 { n /= p }
+    }
+  }
+  if x == 0 { return nil }
+  if n == 1 { return []int{y, x} }
+  return []int{n, x}
+}
+
+
