e85d7611826efc24075d279caa9032da4bba4db5
akar-0 <68073164+akar-0@users.noreply.github.com>
2022-08-12 17:05:38 +0200

    Update lazy_static.rs

--- rust/lazy_static.rs
+++ rust/lazy_static.rs
@@ -1,2 +1,14 @@
 // https://www.codewars.com/kumite/62d41b9fd32b8c005817ee33?sel=62d41b9fd32b8c005817ee33
 // https://www.codewars.com/kumite/62d43b2fc346e300147e537a?sel=62d43b2fc346e300147e537a
+
+
+
+use std::collections::HashSet;
+use lazy_static::lazy_static;
+
+lazy_static! {
+    static ref H: HashSet<char> = {
+        "aeiouAEIOU".chars().collect()
+    };
+}
+
