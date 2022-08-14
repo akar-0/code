f3c4416a7483d226cf1d49379be18a586773e134
akar-0 <68073164+akar-0@users.noreply.github.com>
2022-08-14 19:02:50 +0200

    Update permutations.js

--- js/permutations.js
+++ js/permutations.js
@@ -3,15 +3,15 @@
 // https://www.codewars.com/kata/5865a407b359c45982000036/solutions/javascript
 // https://www.codewars.com/kata/5254ca2719453dcc0b00027d/solutions
 // https://www.codewars.com/kata/56b18992240660a97c00000a/solutions/javascript
-function permutations (arr,l=arr.length,p=[]) {
+// heap algorithm
+function permutations (arr, l=arr.length, p=[]) {
   if (l==1) {p.push(arr.slice())}
-  for (let i=0;i<l;i++) {
+  for (let i = 0; i < l ;i++) {
     permutations(arr,l-1,p)
-    l%2?[arr[0],arr[l-1]]=[arr[l-1],arr[0]]:[arr[i],arr[l-1]]=[arr[l-1],arr[i]]
+    l%2 ? [arr[0] ,arr[l-1]] = [arr[l-1], arr[0]] : [arr[i], arr[l-1]] = [arr[l-1], arr[i]]
   }
   return p
 }
-
 
 // https://stackoverflow.com/questions/9960908/permutations-in-javascript/37580979#37580979
 function permutations(a,l=a.length) {
