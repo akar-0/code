af4f22b994bc8c78ff07cac2fb900ec3db1b4370
akar-0 <68073164+akar-0@users.noreply.github.com>
2022-08-14 12:49:12 +0200

    Update is_square-is_sqrt.rs

--- rust/is_square-is_sqrt.rs
+++ rust/is_square-is_sqrt.rs
@@ -1,2 +1,10 @@
     let is_square = |n| n == ((n as f64).sqrt().round() as u32).pow(2);
     let isqrt = |n| ((n as f64).sqrt() + 1e-9) as u32;
+
+fn is_square(n: u32) -> bool {
+    n == ((n as f64).sqrt().round() as u32).pow(2)
+} 
+
+fn isqrt(n: u32) -> u32 {
+    ((n as f64).sqrt() + 1e-9) as u32
+}
