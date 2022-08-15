cb84a30ad4b83d38a233db3955d008d0a76cded6
akar-0 <68073164+akar-0@users.noreply.github.com>
2022-08-15 20:09:24 +0200

    Update factorial.d

--- d/factorial.d
+++ d/factorial.d
@@ -5,3 +5,17 @@ ulong fact(ulong n) @safe
 {
     return n < 2 ? 1 : n * memoize!fact(n - 1);
 }
+
+import std.bigint, std.conv, std.range, std.algorithm, std.array;
+
+BigInt factorial(ulong n)
+{
+    return iota(2, n+1).fold!((acc, cur) => acc * cur.to!BigInt)(BigInt(1));
+}
+
+
+
+BigInt factorial(ulong n) @safe
+{
+    return n < 2 ? BigInt(1) : BigInt(n) * memoize!factorial(n - 1);
+}
