fd8c01bf212fe2343b969e485cb45fdba264c059
akar-0 <68073164+akar-0@users.noreply.github.com>
2022-08-15 10:12:19 +0200

    Update minMax.d

--- d/minMax.d
+++ d/minMax.d
@@ -16,3 +16,7 @@ Tuple!(ulong, ulong) minMax(ulong[] arr)
               
               )(tuple(arr[0], arr[0]));
 }
+
+
+// Unnamed...
+// https://www.codewars.com/kata/reviews/62ebda4140c3250001f19841/groups/62f0147f39db760001b86b1f
