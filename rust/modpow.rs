1b9cf4bc08eb12ab3c17746ff5d1b3ee39dfe701
akar-0 <68073164+akar-0@users.noreply.github.com>
2022-08-20 10:41:41 +0200

    Update modpow.rs

--- rust/modpow.rs
+++ rust/modpow.rs
@@ -1,1 +1,2 @@
 // https://www.codewars.com/kata/reviews/5c52b82aad5f3f0001266f7c/groups/5eaf13a27e8d1e000198e554
+// https://www.codewars.com/kumite/62fe7ed716c2a900230fdbb1?sel=62fe7ed716c2a900230fdbb1
