6d173bd93f9c7ae452018321a0006b32ef90ec06
akar-0 <68073164+akar-0@users.noreply.github.com>
2022-08-21 11:06:01 +0200

    Update rev_str.go

--- go/rev_str.go
+++ go/rev_str.go
@@ -25,3 +25,10 @@ func reverseSlice(a []string) []string {
     }
     return a
 }
+
+func reverse(bytes []byte) string {
+    for i, j := 0, len(bytes)-1; i < j; i, j = i+1, j-1 {
+        bytes[i], bytes[j] = bytes[j], bytes[i]
+    }
+    return string(bytes)
+}
