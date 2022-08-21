66887eb1466275a1e8a9bb0b4e1bfbac01b8f754
akar-0 <68073164+akar-0@users.noreply.github.com>
2022-08-21 17:23:51 +0200

    Update reverse.go

--- go/reverse.go
+++ go/reverse.go
@@ -5,3 +5,11 @@ func reverse[T comparable](a []T) []T {
     })
   return s
 }
+
+func reverse[T comparable](a []T) []T {
+  res := append([]T{}, a...)
+   for i, j := 0, len(a)-1; i < j; i, j = i+1, j-1 {
+       res[i], res[j] = res[j], res[i]
+   }
+   return res
+}
