dae331e45b621de8e1735bd20cefee15157ffb12
akar-0 <68073164+akar-0@users.noreply.github.com>
2022-08-29 09:13:35 +0200

    Delete D directory

--- D/combinations.d
+++
@@ -1,9 +0,0 @@
-int[] comb(int[] arr, int n) {
-    if (n==1) return arr;
-    int[] o;
-    for (int i = 0 ; i < cast(int)arr.length - n + 1 ; i++) {
-        int h = arr[i];
-        foreach(x;comb(arr[i+1..$], n - 1)) { o ~= x + h;}
-    }
-    return o;
-}
