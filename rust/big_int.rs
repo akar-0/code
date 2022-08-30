3f8039344a352ad580772054618fff43f700a6f6
akar-0 <68073164+akar-0@users.noreply.github.com>
2022-08-30 10:30:33 +0200

    Update big_int.rs

--- rust/big_int.rs
+++ rust/big_int.rs
@@ -9,7 +9,7 @@ use std::str::FromStr;
 
 BigInt::from_str(...).unwrap()
 
-
+BigUint::zero()
 
 // https://www.codewars.com/kata/559b8e46fa060b2c6a0000bf/solutions/rust
 // https://www.codewars.com/kumite/62876ebd0b56c3002576c0bb?sel=62876ebd0b56c3002576c0bb
