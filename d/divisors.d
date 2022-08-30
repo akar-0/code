38bcb4d1c7658cd225a63e4e5f72e721e1a55990
akar-0 <68073164+akar-0@users.noreply.github.com>
2022-08-30 14:53:45 +0200

    Update divisors.d

--- d/divisors.d
+++ d/divisors.d
@@ -19,3 +19,34 @@ uint countDivisors(uint num)
     auto sq = sqrt(cast(double) num);
     return 2 * iota(2, cast(uint) sq + 1).fold!((acc, cur) => acc + !(num%cur))(1) - !(sq % 1.0);
 }
+
+
+int[int] factors(int n)
+{
+    int[] arr;
+    int[int] count;
+    auto c = [4, 2, 4, 2, 4, 6, 2, 6], m = n, p = 7, i = 0;
+    while (m%2 == 0) {
+      arr ~= 2;
+      m /= 2;
+    }
+    while (m%3 == 0) {
+      arr ~= 3;
+      m /= 3;
+    }
+    while (m%5 == 0) {
+      arr ~= 5;
+      m /= 5;
+    }
+    while (m != 1 || p*p <= n) {
+        while (m%p == 0) {
+            arr ~= p;
+            m /= p;
+        }
+        p += c[i];
+        i = (i+1)%8;
+    }
+    if (m > 1) arr ~= m;
+    foreach(x; arr) count[x]++;
+    return count;
+}
