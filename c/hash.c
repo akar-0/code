fbe474b23953b892d35f4ee5a147785ebfbc1239
akar-0 <68073164+akar-0@users.noreply.github.com>
2023-09-06 17:15:27 +0200

    Update hash.c

--- c/hash.c
+++ c/hash.c
@@ -1,1 +1,3 @@
 // https://www.codewars.com/kata/554ca54ffa7d91b236000023/solutions/c
+// use i238 for hash numbers
+// https://www.codewars.com/kata/reviews/64f87cfbb677c50001aea78a/groups/64f89213867587000167d44e
